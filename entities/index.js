86ad1346506d78d7ff0518a4449c27b12f7deb17
luiz.miranda <luizmiranda7@gmail.com>
2015-10-28 14:58:52 -0200

    Fixing db

--- entities/index.js
+++ entities/index.js
@@ -11,7 +11,6 @@ models.forEach(function(model) {
 
 (function(entities){
 	entities.Calendar.hasMany(entities.Interval, {as: 'intervals'});
-	entities.Interval.belongsTo(entities.Calendar, {foreignKey: 'calendar_id'});
 
 	entities.Driver.belongsTo(entities.Person, {as: 'person', foreignKey: 'person_id'});
 	entities.Driver.belongsTo(entities.Calendar, {as: 'calendar', foreignKey: 'calendar_id'});
@@ -20,26 +19,34 @@ models.forEach(function(model) {
 	entities.RoutePoint.belongsTo(entities.Address, {as: 'address', foreignKey: 'address_id'});
 	entities.RoutePoint.belongsTo(entities.Local, {as: 'local', foreignKey: 'local_id'});
 
-	entities.DirectionLeg.belongsTo(entities.RoutePoint, {as: 'initialPoint', foreignKey: 'initial_point_id'});
-	entities.DirectionLeg.belongsTo(entities.RoutePoint, {as: 'finalPoint', foreignKey: 'final_point_id'});
-
 	entities.DeliveryPoint.belongsTo(entities.Calendar, {as: 'calendar', foreignKey: 'calendar_id'});
 	entities.DeliveryPoint.belongsTo(entities.RoutePoint, {as: 'routePoint', foreignKey: 'route_point_id'});
 
 	entities.DistributionCenter.belongsTo(entities.Calendar, {as: 'calendar', foreignKey: 'calendar_id'});
 	entities.DistributionCenter.belongsTo(entities.RoutePoint, {as: 'routePoint', foreignKey: 'route_point_id'});
 
+	entities.DirectionLeg.belongsTo(entities.RoutePoint, {as: 'initialPoint', foreignKey: 'initial_point_id'});
+	entities.DirectionLeg.belongsTo(entities.RoutePoint, {as: 'finalPoint', foreignKey: 'final_point_id'});
+
 	entities.Order.belongsTo(entities.DeliveryPoint, {as: 'deliveryPoint', foreignKey: 'delivery_point_id'});
-	entities.Order.belongsTo(entities.RouteRequest, {foreignKey: 'route_request_id'});
+	entities.Order.belongsTo(entities.DistributionCenter, {as: 'distributionCenter', foreignKey: 'distributionCenter'});
 
 	entities.Route.belongsTo(entities.DistributionCenter, {as: 'distributionCenter', foreignKey: 'dist_center_id'});
 	entities.Route.belongsTo(entities.Vehicle, {as: 'vehicle', foreignKey: 'vehicle_id'});
-	//entities.Route.hasMany(entities.RoutePoint, {as: 'routePoints');
-
-	entities.RouteRequest.hasMany(entities.Order, {as: 'orders'});
+	
 	entities.RouteRequest.belongsTo(entities.DistributionCenter, {as: 'distributionCenter', foreignKey: 'dist_center_id'});
+	
+	// Route n:m Order
+	entities.Route.belongsToMany(entities.Order, {as: 'orders', through: 'RouteOrder'});
+	entities.Order.belongsToMany(entities.Route, {as: 'routes', through: 'RouteOrder'});
 
-	//entities.RouteArea.hasMany(entities.RoutePoint, {as: 'routePoints'});
+	// RouteRequest n:m Order
+	entities.RouteRequest.belongsToMany(entities.Order, {as: 'orders', through:'RouteRequestOrder'});
+	entities.Order.belongsToMany(entities.RouteRequest, {as: 'routeRequests', through: 'RouteOrder'});	
+
+	// RouteArea n:m RoutePoint
+	entities.RouteArea.belongsToMany(entities.RoutePoint, {as: 'routePoints', through:'RouteAreaPoint'});
+	entities.RoutePoint.belongsToMany(entities.RouteArea, {as: 'routeAreas', through:'RouteAreaPoint'});
 
 })(module.exports)
 
